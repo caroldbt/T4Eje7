b671a62526cca4c7802d1359ec11b5487fd2a652
caroldbt <37454302+caroldbt@users.noreply.github.com>
2021-07-10 00:01:24 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace T4Eje7
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*************
+             * Ejercicio 7
+             * ***********/
+
+            Console.WriteLine("Numeros del 1 al 100, bucle for");
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
