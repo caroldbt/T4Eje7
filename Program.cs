683521a9a89423113ae51ceba13c0ac5adabf617
caroldbt <37454302+caroldbt@users.noreply.github.com>
2021-07-10 00:04:30 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,6 +15,43 @@ namespace T4Eje7
             {
                 Console.WriteLine(i);
             }
+            /************
+             * Ejercicio 8
+             * **********/
+
+            Console.WriteLine("Numeros del 1 al 100, bucle while");
+            int j = 1;
+            while (j <= 100)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+
+            /************
+             * Ejercicio 9
+             * **********/
+
+            Console.WriteLine("Numeros pares:");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            /************
+            * Ejercicio 10
+            * **********/
+            Console.WriteLine("Numeros pares o divisibles entre 3");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
         }
     }
 }
